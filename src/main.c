12def681822df5c4d02983a5efdcb25fb0381ae6
Victor Vieux <victorvieux@gmail.com>
2019-10-06 00:40:22 -0700

    use PNGs

--- src/main.c
+++ src/main.c
@@ -27,11 +27,38 @@ void rdp_draw_filled_rectangle_size(int x, int y, int width, int height, uint32_
     rdp_draw_filled_rectangle(x, y, x + width, y + height);
 }
 
+sprite_t *dfs_load_sprite(const char *const path)
+{
+    int fp = dfs_open(path); // Open file in ROM
+    if (fp > 0)
+    {
+        int s = dfs_size(fp);     // Get size of file
+        sprite_t *sp = malloc(s); // Allocate sprite as big as file
+        dfs_read(sp, 1, s, fp);   // Put content of file in sprite
+        dfs_close(fp);            // Close file
+        return sp;
+    }
+    return NULL;
+}
+
+void rdp_draw_sprite_with_texture(sprite_t *sprite, int x, int y)
+{
+    // Set RDP in texture mode
+    rdp_enable_texture_copy();
+    // Assure RDP is ready for new commands
+    rdp_sync(SYNC_PIPE);
+    // Load sprite in TMEM (texture memory)
+    rdp_load_texture(0, 0, MIRROR_DISABLED, sprite);
+    // Draw sprite
+    rdp_draw_sprite(0, x, y);
+}
+
 typedef struct
 {
     int near;
     bool visible;
     bool bomb;
+    bool flag;
 } cell_t;
 
 typedef struct
@@ -40,41 +67,36 @@ typedef struct
     int y;
 } cursor_t;
 
-void draw_board(display_context_t disp, cell_t board[10][10], cursor_t cursor)
+void draw_board(display_context_t disp, cell_t board[10][10], cursor_t cursor, sprite_t *sprites[14], bool gameover)
 {
-    graphics_set_color(DARK_GREY, 0);
-
     for (int x = 0; x < 10; x++)
     {
         for (int y = 0; y < 10; y++)
         {
-            rdp_draw_filled_rectangle_size(60 + x * 20, 20 + y * 20, 20, 20, BLACK);
-
-            if (!board[x][y].visible)
-            {
-                rdp_draw_filled_rectangle_size(60 + x * 20 + 2, 20 + y * 20 + 2, 20 - 4, 20 - 4, DARK_GREY);
-            }
+            if (!gameover && board[x][y].flag)
+                rdp_draw_sprite_with_texture(sprites[11], 60 + x * 20, 20 + y * 20);
+            else if (!gameover && !board[x][y].visible)
+                rdp_draw_sprite_with_texture(sprites[12], 60 + x * 20, 20 + y * 20);
             else
             {
                 if (board[x][y].bomb)
-                    rdp_draw_filled_rectangle_size(60 + x * 20 + 2, 20 + y * 20 + 2, 20 - 4, 20 - 4, RED);
+                {
+                    if (cursor.x == x && cursor.y == y)
+                        rdp_draw_sprite_with_texture(sprites[10], 60 + x * 20, 20 + y * 20);
+
+                    else
+                        rdp_draw_sprite_with_texture(sprites[9], 60 + x * 20, 20 + y * 20);
+                }
                 else
                 {
-                    rdp_draw_filled_rectangle_size(60 + x * 20 + 2, 20 + y * 20 + 2, 20 - 4, 20 - 4, LIGHT_GREY);
-
-                    if (board[x][y].near)
-                    {
-                        graphics_set_color(DARK_GREY, 0);
-                        rdp_detach_display();
-                        graphics_draw_character(disp, 60 + x * 20 + 8, 20 + y * 20 + 8, '0' + board[x][y].near);
-                        rdp_attach_display(disp);
-                    }
+                    rdp_draw_sprite_with_texture(sprites[board[x][y].near], 60 + x * 20, 20 + y * 20);
                 }
             }
 
             if (cursor.x == x && cursor.y == y)
             {
-                rdp_draw_filled_rectangle_size(60 + x * 20 + 4, 20 + y * 20 + 4, 20 - 8, 20 - 8, WHITE);
+                rdp_draw_sprite_with_texture(sprites[13], 60 + x * 20, 20 + y * 20);
+                //rdp_draw_filled_rectangle_size(60 + x * 20 + 4, 20 + y * 20 + 4, 20 - 8, 20 - 8, WHITE);
             }
         }
     }
@@ -150,7 +172,7 @@ int main()
 
     display_init(RESOLUTION_320x240, DEPTH_16_BPP, 2, GAMMA_NONE, ANTIALIAS_RESAMPLE);
     display_context_t disp = 0;
-
+    dfs_init(DFS_DEFAULT_LOCATION);
     rdp_init();
 
     // Remove any clipping windows
@@ -165,22 +187,40 @@ int main()
     cursor_t cursor = {0};
     bool gameover = false;
 
+    sprite_t *sprites[14];
+    sprites[0] = dfs_load_sprite("/gfx/0.sprite");
+    sprites[1] = dfs_load_sprite("/gfx/1.sprite");
+    sprites[2] = dfs_load_sprite("/gfx/2.sprite");
+    sprites[3] = dfs_load_sprite("/gfx/3.sprite");
+    sprites[4] = dfs_load_sprite("/gfx/4.sprite");
+    sprites[5] = dfs_load_sprite("/gfx/5.sprite");
+    sprites[6] = dfs_load_sprite("/gfx/6.sprite");
+    sprites[7] = dfs_load_sprite("/gfx/7.sprite");
+    sprites[8] = dfs_load_sprite("/gfx/8.sprite");
+    sprites[9] = dfs_load_sprite("/gfx/bomb.sprite");
+    sprites[10] = dfs_load_sprite("/gfx/bomb_hit.sprite");
+    sprites[11] = dfs_load_sprite("/gfx/flag.sprite");
+    sprites[12] = dfs_load_sprite("/gfx/hidden.sprite");
+    sprites[13] = dfs_load_sprite("/gfx/cursor.sprite");
+
     init_board(board);
 
     while (true)
     {
-        // get controllers
         controller_scan();
 
         struct controller_data keys = get_keys_down();
-        if (keys.c[0].right)
-            cursor.x = (cursor.x + 1) % 10;
-        if (keys.c[0].left)
-            cursor.x = (cursor.x == 0 ? 9 : cursor.x - 1);
-        if (keys.c[0].down)
-            cursor.y = (cursor.y + 1) % 10;
-        if (keys.c[0].up)
-            cursor.y = (cursor.y == 0 ? 9 : cursor.y - 1);
+        if (!gameover)
+        {
+            if (keys.c[0].right)
+                cursor.x = (cursor.x + 1) % 10;
+            if (keys.c[0].left)
+                cursor.x = (cursor.x == 0 ? 9 : cursor.x - 1);
+            if (keys.c[0].down)
+                cursor.y = (cursor.y + 1) % 10;
+            if (keys.c[0].up)
+                cursor.y = (cursor.y == 0 ? 9 : cursor.y - 1);
+        }
 
         // update game state
         if (keys.c[0].A)
@@ -188,6 +228,10 @@ int main()
             board[cursor.x][cursor.y].visible = true;
             if (board[cursor.x][cursor.y].bomb)
                 gameover = true;
+        }
+        else if (keys.c[0].B)
+        {
+            board[cursor.x][cursor.y].flag = true;
         }
         else if (keys.c[0].start)
         {
@@ -200,7 +244,6 @@ int main()
         while (!(disp = display_lock()))
             ;
 
-        // update display
         rdp_attach_display(disp);
 
         if (gameover)
@@ -208,7 +251,7 @@ int main()
         else
             rdp_draw_filled_rectangle_size(0, 0, 320, 240, WHITE);
 
-        draw_board(disp, board, cursor);
+        draw_board(disp, board, cursor, sprites, gameover);
 
         rdp_detach_display();
 
